bcde3ccc596dfaa23c11cb5a2b2f4d5543d7cba1
jyoshida <jyoshida@gifu-u.ac.jp>
2015-03-04 23:33:17 +0900

    test OK

--- CRangeEnergy_Test/CRangeEnergy_Test.cpp
+++ CRangeEnergy_Test/CRangeEnergy_Test.cpp
@@ -9,7 +9,7 @@
 
 //http://stackoverflow.com/questions/17333/most-effective-way-for-float-and-double-comparison
 bool AreSame(double a, double b) {
-	return std::fabs(a - b) < std::numeric_limits<double>::epsilon();
+	return std::fabs(a - b) < 1E-8;//std::numeric_limits<double>::epsilon();
 }
 
 
@@ -22,7 +22,7 @@ int main(void){
 	double densityEM = 3.6;
 	
 	for(int i=0; i<5; i++){
-		for(int Range = 0;  Range<10; Range++){
+		for(double Range = 0.1;  Range<1000; Range+=5.0){
 			CRangeEnergy cre;
 			double a = cre.GetKineticEnergyFromRange(Mass[i], Range*1.0, Z[i], densityEM);
 			double b = function0(Mass[i], Range, Z[i], densityEM, 0.884);//r=0.884;
